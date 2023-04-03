c94c35fe347482018a0bc48be03f2f704525dcc9
Gerard Yap <cheekian.yap@aiesec.net>
2023-04-03 21:29:50 +0800

    parse tcp header

--- Makefile
+++ Makefile
@@ -1,4 +1,15 @@
 .PHONY: run
 run:
-	apt update && apt install -y iproute2 iputils-ping
+	# apt update && apt install -y iproute2 iputils-ping
 	cargo run
+	# /src/target/debug/tcp &
+	# pid=$!
+
+	# # set up tun0 nic
+	# ip addr add 192.168.0.1/24 dev tun0
+	# ip link set up dev tun0
+
+	# # send a packet to tun0
+	# ping -I tun0 192.168.0.5
+
+	# wait $pid
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,60 @@ use tun_tap;
 
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
-    let mut buf = [0u8; 1500];
-    let n_bytes = nic.recv(&mut buf[..]);
-    eprintln!("read {:?} bytes", n_bytes);
+    let mut buf = [0u8; 1504];
+    loop {
+        let n_bytes = nic.recv(&mut buf[..])?;
+        let eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
 
+        if eth_proto != 2048 {
+            // not ipv4
+            continue;
+        }
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..n_bytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+
+                if proto != 0x06 {
+                    // not tcp
+                    continue;
+                }
+                eprintln!(
+                    "{:?} -> {:?} {:?}b of protocol {:?}",
+                    src,
+                    dst,
+                    p.payload_len(),
+                    proto
+                );
+
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + p.slice().len()..]) {
+                    Ok(p) => {
+                        eprintln!(
+                            "{:?} -> {:?} {:?}b of protocol {:?} to port {:?}",
+                            src,
+                            dst,
+                            p.slice().len(),
+                            proto,
+                            p.destination_port()
+                        );
+                    }
+                    Err(e) => {
+                        eprintln!("ignoring packet {:?}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("ignoring packet {:?}", e);
+            }
+        }
+
+        eprintln!(
+            "read {:?} bytes, flags: {:?}, proto: {:?}",
+            n_bytes, eth_flags, eth_proto
+        );
+    }
     Ok(())
 }
